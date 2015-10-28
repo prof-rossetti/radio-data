abe41ad755b38e61787a86e621f8e68fc01ff4b1
MJ Rossetti (@s2t2) <s2t2mail+git@gmail.com>
2015-10-28 15:01:14 -0400

    renaming attributes

--- generate_reports.sql
+++ generate_reports.sql
@@ -1,15 +1,24 @@
 -- run this script after loading the data into a dbms
 -- ... to produce datasets for use in charting, statistical analysis, or other decision-making purposes
 
--- generate listener_activity_report.csv
+/*
+
+generate 'activity_report.csv' file:
+
+*/
+
 SELECT
   'listener_full_name'
   ,'listener_email_address'
-  ,'listen_started_at'
+  ,'started_listening_at'
   ,'song_title'
-  ,'song_artist_name'
+  ,'artist_name'
   ,'song_duration_ms'
-  ,'song_year_recorded'
+  ,'year_recorded'
+  ,'feedback'
+  ,'feedback_given_at'
+  ,'skipped'
+  ,'skipped_at'
 UNION ALL
 SELECT
   l.full_name AS listener_full_name
@@ -17,13 +26,19 @@ SELECT
   ,p.started_playing_at AS listen_started_at
   ,s.title AS song_title
   ,s.artist_name AS song_artist_name
-  ,s.duration_milliseconds AS song_duration_ms
+  ,s.duration_milliseconds AS song_duration
   ,s.year_recorded AS song_year_recorded
-FROM listeners l
-INNER JOIN plays p ON l.id = p.listener_id
-INNER JOIN songs s ON s.id = p.song_id
+  ,th.thumb_type
+  ,th.thumb_pressed_at
+  ,IF(sk.skipped_at IS NULL,0,1) AS skipped
+  ,sk.skipped_at
+FROM plays p
+JOIN listeners l ON l.id = p.listener_id
+JOIN songs s ON s.id = p.song_id
+LEFT JOIN thumbs th ON th.play_id = p.id
+LEFT JOIN skips sk ON sk.play_id = p.id
 -- ORDER BY l.full_name, s.title, s.artist_name, p.started_playing_at DESC
-INTO OUTFILE '~/projects/gwu-business/radio-DATA/reports/listener_activity_report.csv'
+INTO OUTFILE '~/projects/gwu-business/radio-data/reports/activity_report.csv'
   FIELDS
     TERMINATED BY ','
     ENCLOSED BY '"'
