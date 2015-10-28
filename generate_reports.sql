486a5a81b44c3d0508e8ed3a3bbcbe1158a4e7c1
MJ Rossetti (@s2t2) <s2t2mail+git@gmail.com>
2015-10-28 14:08:50 -0400

    adding header row to report

--- generate_reports.sql
+++ generate_reports.sql
@@ -3,6 +3,15 @@
 
 -- generate listener_activity_report.csv
 SELECT
+  'listener_full_name'
+  ,'listener_email_address'
+  ,'listen_started_at'
+  ,'song_title'
+  ,'song_artist_name'
+  ,'song_duration_ms'
+  ,'song_year_recorded'
+UNION ALL
+SELECT
   l.full_name AS listener_full_name
   ,l.email_address AS listener_email_address
   ,p.started_playing_at AS listen_started_at
@@ -13,7 +22,7 @@ SELECT
 FROM listeners l
 INNER JOIN plays p ON l.id = p.listener_id
 INNER JOIN songs s ON s.id = p.song_id
-ORDER BY l.full_name, s.title, s.artist_name, p.started_playing_at DESC
+-- ORDER BY l.full_name, s.title, s.artist_name, p.started_playing_at DESC
 INTO OUTFILE '~/projects/gwu-business/radio-DATA/reports/listener_activity_report.csv'
   FIELDS
     TERMINATED BY ','
